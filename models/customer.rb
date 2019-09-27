009feebcd6a9c788b295aa5f7bc293e47cd11fe4
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-09-27 13:05:51 +0100

    Added save and delete_all  methods for 3 classes

---
+++ models/customer.rb
@@ -0,0 +1,41 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader(:id)
+  attr_accessor(:name, :funds)
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds']
+  end
+
+  def save()
+    sql = "INSERT INTO customers
+    (
+      name,
+      funds
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id;"
+    values = [@name, @funds]
+    customer = SqlRunner.run( sql, values ).first
+    @id = customer['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    customer_data = SqlRunner.run(sql)
+    return Customer.map_items(customer_data)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
+
+end
---
+++ models/film.rb
@@ -0,0 +1,41 @@
+require_relative("../db/sql_runner")
+
+class Film
+
+  attr_reader(:id)
+  attr_accessor(:title, :price)
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @price = options['price']
+  end
+
+  def save()
+    sql = "INSERT INTO films
+    (
+    title,
+    price
+    )
+    VALUES
+    ($1, $2)
+    RETURN id"
+    values = [@title, @price]
+    film = SqlRunner.run(sql, values).first
+    @id = film['id'].to_i
+  end
+
+
+  def self.all()
+    sql = "SELECT * FROM films"
+    film_data = SqlRunner.run(sql)
+    return Customer.map_items(film_data)
+  end
+
+
+  def self.delete_all()
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end
+
+end
